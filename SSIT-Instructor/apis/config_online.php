8485d69b8e12a1e123d8f1fe05272ad188cae254
aruntejamenda <aruntejamenda@gmail.com>
2022-07-28 12:32:25 +0530

    add_courses 2st commit

---
+++ SSIT-Instructor/apis/config_online.php
@@ -0,0 +1,14 @@
+<?php
+    $host = "localhost";
+    $username="id19331295_aruntejamenda";
+    $dbname = "id19331295_ssit";
+    $password = "@#%+53tedFyy";
+    $con = mysqli_connect($host,$username,$password,$dbname);
+    if($con){
+        // echo "<script>alert('Connected..!!')</script>";
+        //echo "connected";
+    }else{
+        // echo "<script>alert('Not Connected..!!')</script>";
+        echo "<h1>db connection problem</h1>";
+    }
+?>
